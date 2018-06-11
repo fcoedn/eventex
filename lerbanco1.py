6dec76071cfcf72ef9940a38c33a22af0001be66
sergio <sergio.sousa@gmail.com>
2018-06-11 18:26:38 -0300

    teste conexao BD

---
+++ lerbanco1.py
@@ -0,0 +1,47 @@
+"""
+ Francisco Filho
+ Faz conexao
+ Faz leitura
+ Exibe dados em tela caracter (ZIM)
+  11.06.2018  18:10
+"""
+import curses
+import psycopg2
+
+conn = psycopg2.connect("host=localhost dbname=dbaxxx user=uuuuu password=ppppp")
+cur = conn.cursor()
+cur.execute('SELECT * FROM numerarec')
+umreg = cur.fetchone()
+todosreg = cur.fetchall()
+
+# print(one)
+# print(all)
+# for registro in all:
+#     print(registro)
+
+myscreen = curses.initscr()
+myscreen.border(2)
+myscreen.addstr(2, 20, "Listagem da Tabela NumeraRec")
+myscreen.addstr(3, 10, "Loja Seq   Recibo")
+myscreen.addstr(3, 40, "Loja Seq   Recibo")
+
+lin = 4
+col = 10
+for registro in todosreg:
+    myscreen.addstr(lin, col, registro[0] + "    " + str(registro[1]) + "    " + str(registro[2]))
+    if col == 10:
+       col = 40
+    else:
+       lin = lin + 1
+       col = 10
+
+myscreen.addstr(10, 60, "Q Tecla...Para Sair")
+myscreen.refresh()
+myscreen.getch()
+curses.endwin()
+conn.close()
+
+
+
+
+
